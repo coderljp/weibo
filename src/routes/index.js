8bae65ae712118834f1e750f37185da64bbb4df5
coderljp <474426277@qq.com>
2020-07-20 12:38:23 +0800

    all code

---
+++ src/routes/index.js
@@ -0,0 +1,17 @@
+const router = require('koa-router')()
+
+router.get('/', async (ctx, next) => {
+  await ctx.render('index', {
+    title: 'Hello Koa 2!'
+  })
+})
+
+router.get('/json', async (ctx, next) => {
+  ctx.body = {
+    title: 'koa2 json'
+  }
+})
+
+router.get('')
+
+module.exports = router
